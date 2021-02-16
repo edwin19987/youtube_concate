a6db2ebb8b61b4e25f4c4ae5bdd0dcd2450e3fe8
hsu <edwin1998516@gmail.com>
2021-02-16 17:10:42 +0800

    Finished downloading videos

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,10 +1,13 @@
 
 from pipline.pipline import Pipline
 from pipline.steps.get_video_list import GetVideoList
+from pipline.steps.initialize_yt import InitializeYT
 from pipline.steps.download_captions import DownloadCaptions
 from pipline.steps.read_caption import ReadCaption
 from pipline.steps.preflight import Preflight
 from pipline.steps.postflight import Postflight
+from pipline.steps.search import Search
+from pipline.steps.download_video import DownloadVideos
 from utils import Utils
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
@@ -13,14 +16,18 @@ CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 def main():  # pipline pattern
     inputs = {
         'channel_id': CHANNEL_ID,
+        'search_word': 'incredible', 
     }
 
     steps = [  # 要執行的class
         Preflight(),
         GetVideoList(),
+        InitializeYT(),
         DownloadCaptions(),
         ReadCaption(),
-        Postflight()
+        Search(),
+        DownloadVideos(),
+        Postflight(),
     ]
     utils = Utils()
 
--- yt_concate/pipline/steps/read_caption.py
+++ yt_concate/pipline/steps/read_caption.py
@@ -1,14 +1,12 @@
-import os
-from pprint import pprint
 from .step import Step
-from .settings import CAPTIONS_DIR
 
 class ReadCaption(Step):
     def process(self, data, inputs, utils):
-        data = {}
-        for caption_file in os.listdir(CAPTIONS_DIR):
-            captions = {}  #裝時間與字幕的字典
-            with open(os.path.join(CAPTIONS_DIR,caption_file),'r') as f: #因為乎要檔案的main.py是不同資料夾要對path做處理
+        for yt in data:
+            if not utils.caption_file_exist(yt):
+                continue
+            captions={}
+            with open(yt.caption_filepath, 'r') as f: #因為乎要檔案的main.py是不同資料夾要對path做處理
                 # print(f)
                 time_line = False
                 time = None
@@ -22,6 +20,6 @@ class ReadCaption(Step):
                         caption = line.strip()
                         captions[caption] = time  #用字幕當key的原因是: 當用forloop字典時，return的值會是key
                         time_line = False
-            data[caption_file] = captions
-        pprint(data)
+            yt.captions = captions
+
         return data
